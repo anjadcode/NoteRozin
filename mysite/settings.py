c40ce0c45d4b65a11bd8c019c2676ec18942e9c3
mrozinanjad <mrozinanjad88@gmail.com>
2025-06-17 19:18:11 +0700

    fix this

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,11 +133,9 @@ STATIC_URL = 'static/'
 # settings.py
 #STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join (BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [BASE_DIR/'static']
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 
 # Default primary key field type
